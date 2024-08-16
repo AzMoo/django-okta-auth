84651ce64efb54e8c063b3b7ab4cd60a84af4459
Timothy Williams <Timothy.Williams1@resy.com>
2024-08-16 13:56:41 -0400

    Login tags to use Resy urls.

--- okta_oauth2/urls.py
+++ okta_oauth2/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url("login/", views.login, name="login"),
-    url("oauth2/callback/", views.callback, name="callback"),
+    url(r"^/2/os/resy-admin/okta/", views.login, name="login"),
+    url(r"^/2/os/resy-admin/okta-auth", views.callback, name="callback"),
     url("logout/", views.logout, name="logout"),
 ]
