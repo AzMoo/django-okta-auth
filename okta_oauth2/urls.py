d16b38ad55cb77d00b45a788025a321be7894d6d
Timothy Williams <Timothy.Williams1@resy.com>
2024-08-21 10:15:38 -0400

    URL Changes.

--- okta_oauth2/urls.py
+++ okta_oauth2/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r"^okta/", views.login, name="login"),
-    url(r"^okta-auth/", views.callback, name="callback"),
+    url("okta/", views.login, name="login"),
+    url("okta-auth/", views.callback, name="callback"),
     url("logout/", views.logout, name="logout"),
 ]
